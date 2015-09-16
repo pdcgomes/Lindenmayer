5ae84ebc60a2c46d98f24303f2ef64ffca833e32
Pedro Gomes <pdcgomes@gmail.com>
2015-09-16 13:48:48 -0400

    [CHANGED] updated project settings to Xcode 7

---
+++ Lindenmayer/Controllers/TemplatePickerController.swift
@@ -0,0 +1,13 @@
+//
+//  TemplatePickerController.swift
+//  Lindenmayer
+//
+//  Created by Pedro Gomes on 13/06/2015.
+//  Copyright © 2015 Pedro Gomes. All rights reserved.
+//
+
+import UIKit
+
+class TemplatePickerController : UITableViewController {
+    
+}
